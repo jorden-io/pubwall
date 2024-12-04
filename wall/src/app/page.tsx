5ce115ac8992b1d62578d4f614e8221dea44b22b
JorbyWasTaken <jordenrodriguez1004@yahoo.com>
2024-12-03 18:07:41 -0600

    upload test 4

--- wall/src/app/page.tsx
+++ wall/src/app/page.tsx
@@ -35,11 +35,12 @@ export default function Home() {
   };
   const subMessage = async (data: string) => {
     threeTries = 0;
+    const url = blob?.url;
     const body = {
       data,
       name: localStorage.getItem("name"),
       gender: localStorage.getItem("gender"),
-      url: blob?.url
+      url: url
     }; 
     await fetch(
       "https://jktecbt034.execute-api.us-east-2.amazonaws.com/api/message",

e9a6974238abf23442f60c7850b488c796cccf18
JorbyWasTaken <jordenrodriguez1004@yahoo.com>
2025-01-19 19:06:58 -0600

    duplicated blob reset

--- wall/src/app/page.tsx
+++ wall/src/app/page.tsx
@@ -65,6 +65,7 @@ export default function Home() {
         body: JSON.stringify(body),
       }
     );
+    setBlob(null);
     fetchMessags().then(() => {
       document
         .getElementById("hiddenp")
